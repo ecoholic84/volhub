d1d5ada1c470a98605cb3c37d524dd335115ab8f
Akash A <akashanil084@gmail.com>
2024-08-24 17:12:14 +0530

    Content Change

--- index.php
+++ index.php
@@ -98,8 +98,84 @@
                 <div class="container px-6 py-32 mx-auto md:text-center md:px-4">
                     <h1
                         class="text-4xl font-extrabold leading-none leading-10 tracking-tight text-white sm:text-5xl md:text-6xl xl:text-7xl">
-                        <span class="block">Nothing Great is</span> <span
-                            class="relative inline-block mt-3 text-white">Made Alone</span>
+                        <span class="block">Nothing great is</span> <span
+                            class="relative inline-block mt-3 text-white">
+                        <!-- Typing Effect -->
+
+                        <div 
+    x-data="{
+        text: '',
+        textArray : ['made alone', 'achieved in isolation', 'built without teamwork'],
+        textIndex: 0,
+        charIndex: 0,
+        typeSpeed: 110,
+        cursorSpeed: 550,
+        pauseEnd: 1500,
+        pauseStart: 20,
+        direction: 'forward',
+    }" 
+    x-init="$nextTick(() => {
+        let typingInterval = setInterval(startTyping, $data.typeSpeed);
+        
+        function startTyping(){
+            let current = $data.textArray[ $data.textIndex ];
+            
+            // check to see if we hit the end of the string
+            if($data.charIndex > current.length){
+                    $data.direction = 'backward';
+                    clearInterval(typingInterval);
+                    
+                    setTimeout(function(){
+                        typingInterval = setInterval(startTyping, $data.typeSpeed);
+                    }, $data.pauseEnd);
+            }   
+                
+            $data.text = current.substring(0, $data.charIndex);
+            
+            if($data.direction == 'forward')
+            {
+                $data.charIndex += 1;
+            } 
+            else 
+            {
+                if($data.charIndex == 0)
+                {
+                    $data.direction = 'forward';
+                    clearInterval(typingInterval);
+                    setTimeout(function(){
+                        $data.textIndex += 1;
+                        if($data.textIndex >= $data.textArray.length)
+                        {
+                            $data.textIndex = 0;
+                        }
+                        typingInterval = setInterval(startTyping, $data.typeSpeed);
+                    }, $data.pauseStart);
+                }
+                $data.charIndex -= 1;
+            }
+        }
+                    
+        setInterval(function(){
+            if($refs.cursor.classList.contains('hidden'))
+            {
+                $refs.cursor.classList.remove('hidden');
+            } 
+            else 
+            {
+                $refs.cursor.classList.add('hidden');
+            }
+        }, $data.cursorSpeed);
+
+    })"
+    class="flex items-center justify-center mx-auto text-center max-w-7xl">
+    <div class="relative flex items-center justify-center h-auto">
+        <p class="text-7xl font-white leading-tight" x-text="text"></p>
+        <span class="absolute right-0 w-2 -mr-2 bg-black h-3/4" x-ref="cursor"></span>
+    </div>
+</div>
+                        
+                    
+                        </span>
                     </h1>
                     <p
                         class="mx-auto mt-6 text-sm text-left text-gray-200 md:text-center md:mt-12 sm:text-base md:max-w-xl md:text-lg xl:text-xl">
