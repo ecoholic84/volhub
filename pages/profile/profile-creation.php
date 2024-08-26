1bce24113520c3e28a0bfa2e39d782b255eafa5b
Akash A <akashanil084@gmail.com>
2024-08-26 23:19:36 +0530

    Content Change

--- pages/profile/profile-creation.php
+++ pages/profile/profile-creation.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pines UI</title>
+    <title>Profile Creation</title>
     <style>[x-cloak]{display:none}</style>
     <!-- Include the Alpine library on your page -->
     <script src="https://unpkg.com/alpinejs" defer></script>
